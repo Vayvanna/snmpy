202763de8f87fff10748979d59637f73b944c04b
vayvanna <raedsouissi1@gmail.com>
2025-05-22 13:09:42 +0000

    created main files

--- app/routes.py
+++ app/routes.py
@@ -1,3 +1,5 @@
-#has the web routes on /index, on /api/sites etc..
-#
-#
+# handles the pages(like html pages) and APIs endpoints within the site
+# e.g: @app.route("/")
+#    : @app.route("/api/sites")
+
+# it'll have functions that react to when someone opens a specific url.
---
+++ core/scheduler.py
@@ -0,0 +1,3 @@
+# schedules ping/snmp polls 
+# updates their status in db.
+# it'll read from ../config/sites.json and then pings and waits 60secs for example.
