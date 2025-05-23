b7212b7c29ce8b32dd57719eef46bd874d700dd8
vayvanna <raedsouissi1@gmail.com>
2025-05-23 17:40:44 +0000

    auto updates web, both tables work & get auto updated & the admin panel seems to work just fine.

--- db/models.py
+++ db/models.py
@@ -7,7 +7,14 @@
 # basically here, this models.py is the translator of the ORM(object relational M smth) which is SQLAlchemy cuz raw sql is annoying.
 
 # the stuff is creating two tables.
+import datetime
 from app.extensions import db
+# from datetime import datetime
+from datetime import datetime, timedelta, timezone
+from sqlalchemy.types import DateTime
+from sqlalchemy.dialects.postgresql import TIMESTAMP
+tz_utc_plus_1 = timezone(timedelta(hours=1))
+# timestamp = db.Column(TIMESTAMP(timezone=True), default=lambda: datetime.now(tz_utc_plus_1))
 
 class Site(db.Model):
     __tablename__ = 'sites'
@@ -18,7 +25,25 @@ class Site(db.Model):
     longitude = db.Column(db.Float)
     location = db.Column(db.String(100))
     snmp_community = db.Column(db.String(50))
-    status = db.Column(db.String(10))  # optional, or compute live
+    status = db.Column(db.String(10))  # compute live
 
-# class Logs(db.Model):
-#     __talbename__ ='Logs'
+    def __repr__(self):
+        return f"<Site {self.name}>"
+
+    def __str__(self):  # ✅ Flask-Admin uses this!
+        return self.name
+
+    
+default_time = lambda: datetime.utcnow() + timedelta(hours=1)
+
+class SiteLogs(db.Model):
+    __tablename__ = 'sitelogs'  # ✅ corrected typo
+
+    id = db.Column(db.Integer, primary_key=True)
+    site_id = db.Column(db.Integer, db.ForeignKey('sites.id'))
+    # timestamp = db.Column(TIMESTAMP(timezone=True), default=lambda: datetime.now(tz_utc_plus_1))
+    timestamp = db.Column(db.DateTime, default=default_time)
+
+    status = db.Column(db.String(10))
+
+    site = db.relationship('Site', backref='logs')
