860c412abbaf9b338e5f2eb6a2c97680f4a06716
vayvanna <raedsouissi1@gmail.com>
2025-05-23 08:55:15 +0000

    b4 advancing 5/23

---
+++ db/__init__.py
@@ -0,0 +1,5 @@
+# Python only treats folders as "packages" if they have an __init__.py
+
+# When you run a script directly, Python doesn't automatically include sibling folders in the import path — unless it's run from the right location or with PYTHONPATH
+
+# thats why this file exists.
