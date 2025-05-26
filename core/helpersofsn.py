8da710fb44d8660834cfd06751f63bd8b42f3d95
vayvanna <raedsouissi1@gmail.com>
2025-05-26 01:05:07 +0000

    snmp works so well!! backend should be good, added 3 tables, tested with snmpsim, all backend + db is done + auto sync even if id/oid is there.

--- core/helpersofsn.py
+++ core/helpersofsn.py
@@ -4,12 +4,18 @@ from pysnmp.hlapi import (
     ObjectType, ObjectIdentity
 )
 
-def snmp_get(ip, community, oid, timeout=1, retries=1):
-    """Perform SNMP GET and return value as string or None."""
+
+
+
+
+
+
+def snmp_get(ip, community, oid, port=161, timeout=1, retries=1):
+    """Perform SNMP GET and return value as string or None if invalid."""
     iterator = getCmd(
         SnmpEngine(),
         CommunityData(community, mpModel=0),  # SNMPv1
-        UdpTransportTarget((ip, 1161), timeout=timeout, retries=retries),
+        UdpTransportTarget((ip, port), timeout=timeout, retries=retries),
         ContextData(),
         ObjectType(ObjectIdentity(oid))
     )
@@ -17,11 +23,35 @@ def snmp_get(ip, community, oid, timeout=1, retries=1):
     errorIndication, errorStatus, errorIndex, varBinds = next(iterator)
 
     if errorIndication or errorStatus:
+        # print("line 26 helpersofsn.py")
         return None
 
     for varBind in varBinds:
-        return str(varBind[1])
+        value = str(varBind[1])
+        if value.strip() in ["", "No Such Object currently exists", "No Such Instance currently exists"]:
+            return None
+        # print(f"[DEBUG] SNMP GET {ip}:{port} {oid} → {value}")
+        return value
+
+# def snmp_get(ip, community, oid, port=161, timeout=1, retries=1):
+#     """Perform SNMP GET and return value as string or None."""
+#     iterator = getCmd(
+#         SnmpEngine(),
+#         CommunityData(community, mpModel=0),  # SNMPv1
+#         UdpTransportTarget((ip, port), timeout=timeout, retries=retries),
+#         ContextData(),
+#         ObjectType(ObjectIdentity(oid))
+#     )
+
+#     errorIndication, errorStatus, errorIndex, varBinds = next(iterator)
+
+#     if errorIndication or errorStatus:
+#         return None
+
+#     for varBind in varBinds:
+#         return str(varBind[1])
 
 
 
 
+#ماذا عن 127.0.0.1؟ هل يجب أن أحفظ ذلك في قناة PY Simulator؟
